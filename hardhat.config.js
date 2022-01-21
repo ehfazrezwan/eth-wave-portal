8b8e2bec06c2426517e41f72316e820007de52ae
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2022-01-21 23:50:52 +0600

    Changed cofig to connect and upload smart contract to blockchain

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,17 @@ require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task(
+  "accounts",
+  "Prints the list of accounts",
+  async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
+    for (const account of accounts) {
+      console.log(account.address);
+    }
   }
-});
+);
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,4 +22,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/xS12ERHUrfH_fUn_51J3vkjK2001f5Rl",
+      accounts: [
+        "77bd296e5f801f72b9798b40cf935e47ab1df637e43351a811fe667049f0cc1e",
+      ],
+    },
+  },
 };
