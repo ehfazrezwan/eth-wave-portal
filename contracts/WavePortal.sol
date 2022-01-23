8c1afcc9281ef712583b2220741abbb79289cf5a
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2022-01-23 16:00:17 +0600

    Updated smart contract to store complete transaction data, and emit events upon a new wave

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,16 +7,36 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
-    constructor() {
-        console.log("My very first smart contract!");
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
     }
 
-    function wave() public {
+    Wave[] waves;
+
+    constructor() {
+        console.log("WavePortal smart contract!");
+    }
+
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved!", msg.sender);
+        console.log("%s waved w/ message %s", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
+        // Optional: Add this line if you want to see the contract print the value!
+        // We'll also print it over in run.js as well.
         console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
